c130b5cfc67779104912b2bc5ad154c348637ce8
Friedrich Lindenberg <friedrich@pudo.org>
2014-11-09 17:47:55 +0200

    dont fetch twice ma, its alright

--- common.py
+++ common.py
@@ -1,7 +1,17 @@
 import os
+import json
 
 DATA_PATH = 'filings'
 
 try:
     os.makedirs(DATA_PATH)
-except: pass
+except:
+    pass
+
+
+def filings():
+    for filename in os.listdir(DATA_PATH):
+        filename = os.path.join(DATA_PATH, filename)
+        with open(filename, 'r') as fh:
+            data = json.load(fh)
+            yield data
--- fetch.py
+++ fetch.py
@@ -11,11 +11,12 @@ URL = 'http://opendatalabs.org/misc/openoil/subsidiaries_1.csv'
 res = requests.get(URL)
 for row in unicodecsv.DictReader(StringIO(res.content)):
     url = row.get('file_url')
+    fn = os.path.join(DATA_PATH, hashlib.sha1(url).hexdigest())
+    if os.path.exists(fn):
+        continue
+
     cn = row.get('company_name')
     r = requests.get(url)
-
-    fn = os.path.join(DATA_PATH, hashlib.sha1(url).hexdigest())
-    print url, fn
     with open(fn, 'wb') as fh:
         row['content'] = r.content
         json.dump(row, fh)
